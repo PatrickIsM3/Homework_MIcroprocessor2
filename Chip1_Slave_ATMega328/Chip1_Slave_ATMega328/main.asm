aad8d0f2324b08a8304d4f6e9fe15bbd0c1dbc72
PatrickIsM3 <117426976+PatrickIsM3@users.noreply.github.com>
2023-12-08 00:06:15 +0700

    Add files via upload

---
+++ Chip1_Slave_ATMega328/Chip1_Slave_ATMega328/main.asm
@@ -0,0 +1,481 @@
+/*;R16 use for setting register
+;R17 is check button and data
+;R20,R21,R22 : delay 
+.org 0x00
+ldi r16,high(RAMEND)
+out SPH,r16
+ldi r16,low(RAMEND)
+out SPL,r16
+call SPI_Slave_Init
+ldi r16,0x0f
+out ddrd,r16
+ldi r16,0xf0
+out portd,r16
+sbi ddrc,0
+sbi portc,0
+;KEYPAD;
+keypad_scan:
+	rcall enter
+	rjmp keypad_scan
+enter:
+
+		REPEAT:	ldi r17,0xFE     ;input port a from pin 4->7 is pull-up resistor
+				out PORTD,r17   ; from 0->3 is output it can be high or low
+								; we need to check all column , then go to row
+				sbic PIND,4     ; if pind 4 is press 
+				rjmp JUMP       ; if not we jump to another rows
+				call DELAY_1MS
+		debounce: sbis pind,4
+				  rjmp debounce
+
+				LDI R17,0x33    ; than we load number 3
+			    rcall SPI_Transmitter
+	
+		JUMP:
+				sbic pind,5    ; pind 5 is press or not
+				rjmp JUMP_1
+				call DELAY_1MS
+		debounce1:	sbis pind,5
+					rjmp debounce1
+		
+				LDI R17,0x34     ; here we load number 4
+				rcall SPI_Transmitter
+
+		JUMP_1: sbic pind,6    
+				RJMP JUMP_2
+				call DELAY_1MS
+		debounce2:	sbis pind,6
+					rjmp debounce2
+				LDI R17,0x42
+				rcall SPI_Transmitter
+				
+		JUMP_2:
+				sbic pind,7
+				RJMP JUMP_3
+				call DELAY_1MS
+		debounce3:	sbis pind,7
+					rjmp debounce3
+
+				LDI R17,0x43
+				rcall SPI_Transmitter
+				
+		JUMP_3:	
+				ldi R17,0xFD      ; here we change other columns
+				out portd,R17    ; all code will have this algorithm afterall 
+				sbic pind,4
+				rjmp JUMP_4
+				call DELAY_1MS
+
+		debounce4:	sbis pind,4
+					rjmp debounce4
+						
+				LDI R17,0x32
+				rcall SPI_Transmitter
+			
+		JUMP_4: 
+				sbic pind,5
+				RJMP JUMP_5
+				call DELAY_1MS
+		debounce5:	sbis pind,5
+					rjmp debounce5
+
+			
+				LDI R17,0x35
+				rcall SPI_Transmitter
+			
+		JUMP_5:	
+				sbic pind,6
+				rjmp JUMP_6
+				call DELAY_1MS
+
+		debounce6:	sbis pind,6
+					rjmp debounce6
+					
+				LDI R17,0x41
+				rcall SPI_Transmitter
+		
+		JUMP_6:
+				sbic pind,7
+				rjmp JUMP_7
+				call DELAY_1MS
+
+		debounce7:	sbis pind,7
+					rjmp debounce7
+					 
+				LDI R17,0x44
+				rcall SPI_Transmitter
+					
+		JUMP_7:
+				LDI R17,0xFB
+				out portd,r17
+				sbic pind,4
+				rjmp JUMP_8
+				call DELAY_1MS
+
+		debounce8:	sbis pind,4
+					rjmp debounce8
+			
+				LDI R17,0x31
+				rcall SPI_Transmitter
+			
+		JUMP_8:
+				sbic pind,5
+				rjmp JUMP_9
+				call DELAY_1MS
+
+		debounce9:	sbis pind,5
+					rjmp debounce9
+
+					
+				LDI R17,0x36
+				rcall SPI_Transmitter
+			
+		JUMP_9:	
+				sbic pind,6
+				rjmp JUMP_10
+				call DELAY_1MS
+
+		debounce10:	sbis pind,6
+					rjmp debounce10
+							
+				LDI R17,0x39
+				rcall SPI_Transmitter
+				
+		JUMP_10:
+				sbic pind,7
+				RJMP JUMP_11
+				call DELAY_1MS
+		debounce11:	sbis pind,7
+					rjmp debounce11
+	
+				LDI R17,0x45
+				rcall SPI_Transmitter
+			
+		JUMP_11:	
+				LDI R17,0xF7
+				OUT portd,R17
+				SBIC pind,4
+				RJMP JUMP_12
+				call DELAY_1MS
+
+		debounce12:	sbis pind,4
+					rjmp debounce12
+			
+				LDI R17,0x30
+				rcall SPI_Transmitter
+			
+		JUMP_12:
+				sbic pind,5
+				rjmp JUMP_13
+				call DELAY_1MS
+
+		debounce13:	sbis pind,5
+					rjmp debounce13
+					
+				LDI R17,0x37
+				rcall SPI_Transmitter
+			
+		JUMP_13:
+				sbic pind,6
+				RJMP JUMP_14
+				call DELAY_1MS
+
+		debounce14:	sbis pind,6
+					rjmp debounce14
+			
+				LDI R17,0x38
+				rcall SPI_Transmitter
+
+		JUMP_14:
+				sbic pind,7
+				RJMP JUMP_15
+				call DELAY_1MS
+
+		debounce15:	sbis pind,7
+					rjmp debounce15
+
+				LDI R17,0x46
+			    rcall SPI_Transmitter
+		JUMP_15:
+				RJMP REPEAT
+				ret
+;SPI_Initial
+SPI_Slave_Init:
+push r16
+ldi r16, (0 << PB2 )|( 0 << PB3)|(1 << PB4)|(0 << PB5)
+out ddrb, r16
+ldi r16, ( 1 << SPE ) | ( 1 << SPR0)
+out SPCR, r16
+ldi	r16	,( 1 << SPI2X)
+out SPSR, r16
+pop r16
+ret
+;SPI-USART-Receciver	
+;Check flag
+SPI_Transmitter:
+cbi PORTC,0
+nop
+nop
+out SPDR,r17
+SPI_T_Wait:
+in r18,SPSR
+sbrs r18,SPIF
+rjmp SPI_t_Wait
+in r17,SPDR
+sbi PORTC,0 
+ret
+
+;DELAY_1MS;
+DELAY_1MS:
+	LDI R20,80
+L1: LDI R21,25
+L2:	DEC R21
+	NOP
+	BRNE L2
+	DEC R20
+	BRNE L1
+	RET
+	*/
+	.equ sck=5
+.equ miso=4
+.equ mosi=3
+.equ ss=2
+.org 0
+ldi r16, high(RAMEND)
+out SPH, r16
+ldi r16, low(RAMEND)
+out SPL, r16
+rcall spi_init
+
+ldi r16,$0f
+out ddrd,r16 ; 0-3 la output 4-7 la input pull-up 
+ldi r16,$f0
+out portd,r16
+
+
+sbi ddrc,0
+sbi portc,0
+loop:
+	rcall xu_ly
+	rjmp loop
+xu_ly:
+
+		LAP:	ldi r17,$fe     ;in port a from pin 4->7 is pull-up resistor
+				out portd,r17   ;so from 0->3 is output it can be high or low
+				sbic PIND,4     ; if pina 4 is press 
+				rjmp NHAY       ; if not we jump to another rows
+				
+				rcall DELAY
+		deb:	sbis pind,4
+				rjmp deb
+
+				LDI R17,$33    ; than we load number 3
+			    rcall spi_transmit
+	
+		NHAY:
+				SBIC PIND,5    ; pina 5 is press or not
+				RJMP NHAY_1
+
+				rcall DELAY
+		deb1:	sbis pind,5
+				rjmp deb1
+		
+				LDI R17,$34     ; here we load number 4
+				rcall spi_transmit
+
+		NHAY_1: SBIC PIND,6    
+				RJMP NHAY_2
+
+				rcall DELAY
+		deb2:	sbis pind,6
+				rjmp deb2
+			
+				LDI R17,$42
+				rcall spi_transmit
+				
+		NHAY_2:
+				SBIC PIND,7
+				RJMP NHAY_3
+				
+				rcall DELAY
+		deb3:	sbis pind,7
+				rjmp deb3
+
+				LDI R17,$43
+				rcall spi_transmit
+				
+		NHAY_3:	
+				LDI R17,$fD      ; here we change other columns
+				OUT PORTD,R17    ; and we check like the code above
+				SBIC PIND,4
+				RJMP NHAY_4
+				
+				rcall DELAY
+		deb4:	sbis pind,4
+				rjmp deb4
+						
+				LDI R17,$32
+				rcall spi_transmit
+			
+		NHAY_4: 
+				SBIC PIND,5
+				RJMP NHAY_5
+				
+				rcall DELAY
+		deb5:	sbis pind,5
+				rjmp deb5
+
+			
+				LDI R17,$35
+				rcall spi_transmit
+			
+		NHAY_5:	
+				SBIC PIND,6
+				RJMP NHAY_6
+				
+				rcall DELAY
+		deb6:	sbis pind,6
+				rjmp deb6
+					
+				LDI R17,$41
+				rcall spi_transmit
+		
+		NHAY_6:
+				SBIC PIND,7
+				RJMP NHAY_7
+				
+				rcall DELAY
+		deb7:	sbis pind,7
+				rjmp deb7
+					 
+				LDI R17,$44
+				rcall spi_transmit
+					
+		NHAY_7:
+				LDI R17,$fB
+				OUT PORTD,R17
+				SBIC PIND,4
+				RJMP NHAY_8
+				
+				rcall DELAY
+		deb8:	sbis pind,4
+				rjmp deb8
+			
+				LDI R17,$31
+				rcall spi_transmit
+			
+		NHAY_8:
+				SBIC PIND,5
+				RJMP NHAY_9
+				
+				rcall DELAY
+		deb9:	sbis pind,5
+				rjmp deb9
+
+					
+				LDI R17,$36
+				rcall spi_transmit
+			
+		NHAY_9:	
+				SBIC PIND,6
+				RJMP NHAY_10
+				
+				rcall DELAY
+		deb10:	sbis pind,6
+				rjmp deb10
+							
+				LDI R17,$39
+				rcall spi_transmit
+				
+		NHAY_10:
+				SBIC PIND,7
+				RJMP NHAY_11
+				
+				rcall DELAY
+		deb11:	sbis pind,7
+				rjmp deb11
+	
+				LDI R17,$45
+				rcall spi_transmit
+			
+		NHAY_11:	
+				LDI R17,$f7
+				OUT PORTD,R17
+				SBIC PIND,4
+				RJMP NHAY_12
+				
+				rcall DELAY
+		deb12:	sbis pind,4
+				rjmp deb12
+			
+				LDI R17,$30
+				rcall spi_transmit
+			
+		NHAY_12:
+				SBIC PIND,5
+				RJMP NHAY_13
+				
+				rcall DELAY
+		deb13:	sbis pind,5
+				rjmp deb13
+					
+				LDI R17,$37
+				rcall spi_transmit
+			
+		NHAY_13:
+				SBIC PIND,6
+				RJMP NHAY_14
+				
+				rcall DELAY
+		deb14:	sbis pind,6
+				rjmp deb14
+			
+				LDI R17,$38
+				rcall spi_transmit
+
+		NHAY_14:
+				SBIC PIND,7
+				RJMP NHAY_15
+				
+				rcall DELAY
+		deb15:	sbis pind,7
+				rjmp deb15
+
+				LDI R17,$46
+			    rcall spi_transmit
+		NHAY_15:
+				RJMP LAP
+ret
+
+spi_transmit:
+	cbi portc,0
+	nop
+	nop
+	nop
+	nop
+	out spdr,r17
+
+	wait_transmit: 
+	in r18,spsr
+	sbrs r18,spif
+	rjmp wait_transmit
+	in r17,spdr
+	sbi portc,0
+	ret
+
+
+spi_init:
+	ldi r16,(1<<miso)|(0<<sck)|(0<<ss)|(0<<mosi)
+	out ddrb,r16
+	ldi r16,(1<<SPE)|(1<<spr0)
+	out spcr,r16
+	ret
+DELAY:
+L3: LDI R21,100 ;1MC
+L1: LDI R20,200 ;1MC
+L2: DEC R20 ;1MC
+NOP ;1MC
+BRNE L2 ;2/1MC
+DEC R21 ;1MC
+BRNE L1 ;2/1MC
+RET
---
+++ Chip2_Master_ATMEGA328P/Chip2_Master_ATMEGA328P/main.asm
@@ -0,0 +1,407 @@
+/*.org 0x0000 ; interrupt vector table
+;PORTC is connect with LCD
+;PORTD is data from keypad
+;PORTB is setting USART and SPI
+ldi r16,(1 << PC2)|(1 << PC3)|(1 << PC4)
+out ddrc,r16
+CBI		PORTC,2			
+CBI		PORTC,3		;set up lcd	
+CBI		PORTC,4	
+call	POWER_RESET_LCD8	
+call	INIT_LCD8
+call SPI_Master_Init
+call USART_Init
+call SPI_Master_Init
+ldi r16,0xFF
+out ddrd,r16
+;When receive data , it will toggle
+cbi ddrc,0
+sbi portc,0
+sbi portb,2
+start:
+call SPI_Transfer
+rjmp start
+SPI_Master_Init:
+push r16
+ldi r16, (1 << PB2 )|( 1 << PB3)|(0 << PB4)|(1 << PB5)
+out DDRB, r16
+ldi r16, ( 1 << SPE ) | ( 1 << MSTR ) | ( 1 << SPR0)
+sts SPCR, r16
+ldi	r16	,( 1 << SPI2X)
+sts SPSR, r16
+pop r16
+ret
+
+;SPI-USART-Transfer
+;Do anything , just set SPCR
+;Check flag
+SPI_Transfer:
+push r17
+;check the data receive or not
+;if yes , give data to SPI Transmit
+check_flag: sbic pinc,0
+			rjmp check_flag
+cbi portb,2
+nop
+out SPDR,r16
+SPI_T_Wait:
+in r18,SPCR
+sbrs r18,SPIF
+rjmp SPI_T_Wait
+;Do anything , just set SPCR
+in r16,spdr
+sbi portb,2
+call USART_Transmit_Data
+mov r20,r16 ; give data to r20 , ready to do
+;if RS clear , give out LCD
+cbi portc,2
+ldi r17,0x01
+call OUT_LCD 
+ldi r16,20
+call DELAY_10MS
+mov r17,r20
+sbi portc,2 
+call OUT_LCD1
+ldi r16,1
+call DELAY_10MS
+ret
+;if Rs set , give data out LCD 
+ ;DELAY_10MS:
+ DELAY_1MS:
+	LDI R20,8
+L1: LDI R21,25
+L2:	DEC R21
+	NOP
+	BRNE L2
+	DEC R20
+	BRNE L1
+	RET
+//DELAY 10MS//
+DELAY_10MS:
+	LDI R22,10
+L3: call DELAY_1MS
+	DEC R22
+	BRNE L3
+	RET
+
+
+;USART Subroutine
+;UART Initialize
+USART_Init:
+; double speed for the USART
+ldi r16,(1 << U2X0)
+sts UCSR0A,r16
+; Open transmit-receive
+ldi r16,(1 << TXEN0)|( 1<< RXEN0)
+sts UCSR0B, r16
+;set frame format :8 data bits , no parity , 1 stop bit
+ldi r16,(1 << UCSZ01)|(1<<UCSZ00)
+sts UCSR0C, r16
+;Set BAUD RATE ( this case is 9600)
+;fosc = 8MHz
+;For any other BAUD RATE, set in r16
+;And check the frequency before calculate
+;BAUD RATE = fosc / (( UBRR+1)*16)
+ldi r16,103 
+sts UBRR0L,r16
+ret
+;UART transmit
+; r17 is the register will receive or send data
+USART_Transmit_Data:	
+	push r17
+USART_T_Wait:
+	 lds r17,UCSR0A
+	 sbrs r17,UDRE0
+	 rjmp USART_T_Wait
+	 sts UDR0,r16
+	 pop r17
+	 ret
+;UART receive
+USART_Receive_Data:	
+	push r17
+USART_R_Wait:
+	 lds r17,UCSR0A
+	 sbrs r17,RXC0
+	 rjmp USART_R_Wait
+	 lds r16,UDR0
+	 pop r17 
+	 ret
+
+INIT_LCD8:                       
+				CBI PORTC,2
+				LDI	R17,0X02
+				CALL OUT_LCD   ; set the cursor to
+				LDI R16,1
+				call DELAY_10MS
+
+				CBI PORTC,2
+				LDI R17,0X01
+				CALL OUT_LCD   ; clear all the content original of lcd
+				LDI R16,20
+				CALL DELAY_10MS
+
+				CBI PORTC,2
+				LDI R17,0X0C
+				RCALL OUT_LCD    ;turn on lcd and turn off cursor
+				LDI R16,1
+				CALL DELAY_10MS
+				RET
+POWER_RESET_LCD8: 
+				LDI		R16,200				
+				CALL	DELAY_10MS		
+			    CBI		PORTC,2
+				LDI		R17,0x30		; we set up lcd 3 times
+				RCALL	OUT_LCD
+				LDI		R16,42
+				call DELAY_10MS
+
+				CBI		PORTC,2
+				LDI		R17,0x30			
+				RCALL	OUT_LCD
+				LDI		R16,2
+				CALL	DELAY_10MS
+
+				CBI		PORTC,2
+				LDI		R17,0x30
+				RCALL	OUT_LCD
+				LDI		R16,2
+				CALL	DELAY_10MS		
+				RET
+OUT_LCD1: 	
+				MOV		R21,R17
+				ANDI    R21,0xF0  ; this one the same code above
+				OUT		PORTD,R21 ; just diffent we don't have ori  
+				SBI		PORTC,4   ; because we want rs=0 for commands
+				CBI		PORTC,4
+
+				LDI		R16,1
+				CALL	DELAY_10MS
+
+				SWAP	R17
+				ANDI	R17,0xF0
+				OUT		PORTD,R17
+				SBI		PORTC,4				
+				CBI		PORTC,4
+
+				LDI		R16,1
+				CALL	DELAY_10MS
+				RET
+
+OUT_LCD: 
+				MOV		R21,R17
+				ANDI    R21,0xF0   ; code here we have value of keypad(0,1,2,3,...,E,F)in assci in r17
+				OUT		PORTD,R21 ;the reason why we have to do ori is make sure rs always =1
+				SBI		PORTC,4   ; this code for high nibble
+				CBI		PORTC,4
+
+				LDI		R16,1
+				CALL	DELAY_10MS
+
+				SWAP	R17
+				ANDI	R17,0xF0
+				OUT		PORTD,R17
+				SBI		PORTC,4				
+				CBI		PORTC,4
+				LDI		R16,1
+				CALL	DELAY_10MS
+				RET
+				*/
+
+.equ sck=5
+.equ miso=4
+.equ mosi=3
+.equ ss=2
+        .EQU	RS=2		;bit RS
+		.EQU	RW=3		;bit RW
+		.EQU	E=4		    ;bit E
+
+.org 0
+
+ldi r16, high(RAMEND)
+out SPH, r16
+ldi r16, low(RAMEND)
+out SPL, r16
+
+ldi r16,$1c
+out ddrc,r16
+
+CBI		PORTC,RS			
+CBI		PORTC,RW		;set up lcd	
+CBI		PORTC,E	
+
+RCALL	POWER_RESET_LCD8	
+RCALL	INIT_LCD8
+
+rcall spi_init
+rcall USART_Init       ; Initialize USART
+rcall spi_init
+ldi r16,$ff
+out ddrd,r16
+
+;cbi ddrc,0           ; tin hieu gui cho master la da co data tu slave
+sbi portc,0
+
+sbi portb,2
+loop:
+	rcall spi_transmit
+
+	rjmp loop
+spi_transmit:
+	check: sbic pinc,0
+	       rjmp check
+
+	cbi portb,2
+	nop
+	out spdr,r16
+	wait_transmit: 
+	in r18,spsr
+	sbrs r18,spif
+	rjmp wait_transmit
+	in r16,spdr    ; r16 chua data tu slave
+	sbi portb,2
+	call USART_SendChar
+	mov r20,r16
+
+	CBI PORTC,RS
+	LDI R17,0X01
+	RCALL OUT_LCD  ; clear display
+	LDI R16,20
+	RCALL DELAY_US
+
+	mov r17,r20
+
+	sbi PORTC,rs
+	rcall XUAT
+	ldi r16,1
+	RCALL	DELAY_US
+	ret
+
+
+spi_init:
+	ldi r16,(1<<sck)|(1<<ss)|(1<<mosi)|(0<<miso)
+	out ddrb,r16
+	ldi r16,(1<<SPE)|(1<<mstr)|(1<<spr0)
+	out spcr,r16
+	ret
+USART_Init:
+    ldi r16, 103           ; Set baud rate for 9600 bps with 1 MHz clock
+    sts UBRR0L, r16       ; Set baud rate low byte
+    ldi r16, (1<< U2X0)  ; Set double speed
+    sts UCSR0A, r16
+    ldi r16, (1 << UCSZ01) | (1 << UCSZ00) ; 8 data bits, no parity, 1 stop bit
+    sts UCSR0C, r16
+    ldi r16, (1 << RXEN0) | (1 << TXEN0)   ; Enable transmitter and receiver
+    sts UCSR0B, r16
+    ret
+
+USART_SendChar:
+    push r17
+    USART_SendChar_Wait:
+    lds r17, UCSR0A
+    sbrs r17, UDRE0       ; Wait for data register to be empty 
+    rjmp USART_SendChar_Wait
+    sts UDR0, r16         ; Send character in r16
+    pop r17
+    ret
+
+USART_ReceiveChar:
+    push r17
+    USART_ReceiveChar_Wait:
+    lds r17, UCSR0A
+    sbrs r17, RXC0        ; Wait for receive complete
+    rjmp USART_ReceiveChar_Wait
+    lds r16, UDR0         ; Store received character in r16
+    pop r17
+    ret
+INIT_LCD8:                       
+				
+				CBI PORTC,RS
+				LDI	R17,0X02
+				RCALL OUT_LCD   ; set the cursor to
+				LDI R16,1
+				RCALL DELAY_US
+
+				CBI PORTC,RS
+				LDI R17,0X01
+				RCALL OUT_LCD   ; clear all the content original of lcd
+				LDI R16,20
+				RCALL DELAY_US
+
+				CBI PORTC,RS
+				LDI R17,0X0C
+				RCALL OUT_LCD    ;turn on lcd and turn off cursor
+				LDI R16,1
+				RCALL DELAY_US
+				RET
+POWER_RESET_LCD8: 
+				LDI		R16,200				
+				RCALL	DELAY_US		
+			    CBI		PORTC,RS
+				LDI		R17,$30		; we set up lcd 3 times
+				RCALL	OUT_LCD
+				LDI		R16,42
+				RCALL	DELAY_US
+
+				CBI		PORTC,RS
+				LDI		R17,$30			
+				RCALL	OUT_LCD
+				LDI		R16,2
+				RCALL	DELAY_US
+
+				CBI		PORTC,RS
+				LDI		R17,$30
+				RCALL	OUT_LCD
+				LDI		R16,2
+				RCALL	DELAY_US		
+				RET
+
+
+XUAT: 
+				MOV		R21,R17
+				ANDI    R21,$F0   ; code here we have value of keypad(0,1,2,3,...,E,F)in assci in r17
+				OUT		PORTD,R21 ;the reason why we have to do ori is make sure rs always =1
+				SBI		PORTC,E   ; this code for high nibble
+				CBI		PORTC,E
+
+				LDI		R16,1
+				RCALL	DELAY_US
+
+				SWAP	R17
+				ANDI	R17,$F0
+				OUT		PORTD,R17
+				SBI		PORTC,E				
+				CBI		PORTC,E
+				LDI		R16,1
+				RCALL	DELAY_US
+
+				RET
+				
+OUT_LCD: 	
+				MOV		R21,R17
+				ANDI    R21,$F0  ; this one the same code above
+				OUT		PORTD,R21 ; just diffent we don't have ori  
+				SBI		PORTC,E   ; because we want rs=0 for commands
+				CBI		PORTC,E
+
+				LDI		R16,1
+				RCALL	DELAY_US
+
+				SWAP	R17
+				ANDI	R17,$F0
+				OUT		PORTD,R17
+				SBI		PORTC,E				
+				CBI		PORTC,E
+
+				LDI		R16,1
+				RCALL	DELAY_US
+				RET
+
+DELAY_US:	MOV	R15,R16		
+			LDI	R16,200			
+L1:			MOV	R14,R16		
+L2:			DEC	R14				
+			NOP					
+			BRNE	L2		
+			DEC		R15				
+			BRNE	L1				
+			RET
